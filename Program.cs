add2194396cc2f73b5ca2ccbd2433dfa3e7d639f
azeezsaliu <azeezsaliu108@gmail.com>
2023-09-01 15:28:00 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student ola = new Student();
+
+            Student obi = new Student(1, "Obinna", 4);
+            Student kim = new Student(2, "Kimbawa", 6);
+
+            /*
+            obi.SaveRecord(1, "Obinna", 4);
+            kim.SaveRecord(2, "Kimbawa", 6);
+            */
+
+            ola.SaveRecord(3, "honda", 32);
+
+            Student.ChangeClassNo(34);
+
+            obi.DisplayInfo();
+            kim.DisplayInfo();
+
+            //obi.SetName("Obinnachukwu");
+            obi.Name = "Obinnachukwu";
+
+            string obiName = obi.Name;
+            Console.WriteLine("Name from getAccessor: {0}", obiName);
+
+
+
+            Console.WriteLine("After name change");
+            obi.DisplayInfo();
+            kim.DisplayInfo();
+
+
+            Organisation o = new Organisation();
+
+            Institution inst = new Institution();
+
+
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExample
+{
+    internal class Student
+    {
+        /*
+        private int id;
+        private string name;
+
+        private static int classNo;
+        */
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public static int ClassNo { get; set; }
+
+
+        public Student()
+        {
+
+        }
+
+        public Student(int id, string name, int classNo)
+        {
+            this.Id = id;
+            this.Name = name;
+            Student.ClassNo = classNo;
+        }
+
+        public void SaveRecord(int id, string name, int classNo)
+        {
+            this.Id = id;
+            this.Name = name;
+            Student.ClassNo = classNo;
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Id: {0}, Name: {1}, ClassNo: {2}", this.Id, Name, Student.ClassNo);
+        }
+
+        /*
+        public string Name
+        {
+            get
+            {
+
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        */
+        /*
+        public void SetName(string n)
+        {
+            name = n;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+        */
+        private bool IsValid(int idValus)
+        {
+            if(idValus != 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static void ChangeClassNo(int classNo)
+        {
+            Student.ClassNo = classNo;
+        }
+
+        ~Student()
+        {
+            Console.WriteLine("{0}, Is destroyed", Name);
+        }
+
+    }
+}
